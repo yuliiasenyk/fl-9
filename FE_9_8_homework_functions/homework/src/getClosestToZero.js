c41ab209e2783ca0adb5a61dd26169e901e3ffbe
Yuliia Senyk <41129257+yuliiasenyk@users.noreply.github.com>
2018-08-23 23:16:42 +0300

    Update getClosestToZero.js

--- FE_9_8_homework_functions/homework/src/getClosestToZero.js
+++ FE_9_8_homework_functions/homework/src/getClosestToZero.js
@@ -2,7 +2,7 @@ function getClosestToZero(...array) {
 	let zeroClose = array[0];
 	for (let i = 0; i < array.length; i++) {
 		if (Math.abs(zeroClose) > array[i]) {
-			zero = array[i];
+			zeroClose = array[i];
 		}
 	}
 	return zeroClose;
